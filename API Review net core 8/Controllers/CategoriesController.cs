15a974a5ec11ee6be33ee496a609f3a365fb7ae5
YaraGh <yaraesmaiel123@gmail.com>
2025-08-22 16:57:46 -0500

    End Points : Get +Post

---
+++ API Review net core 8/Controllers/CategoriesController.cs
@@ -0,0 +1,37 @@
+﻿using API_Review_net_core_8.Data;
+using API_Review_net_core_8.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API_Review_net_core_8.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        public CategoriesController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        private readonly AppDbContext _db;
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetCategories()
+        {
+            var cats = await _db.Categories.ToListAsync();
+            return Ok(cats);
+        }
+      [HttpPost]
+        public async Task<IActionResult> AddCategory(string category)
+        {
+            Category c = new() { Name = category  };
+
+            await _db.Categories.AddAsync(c);
+            _db.SaveChanges();
+            return Ok(c);
+        }
+    }
+}
